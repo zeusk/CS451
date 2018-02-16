54afd688e3c29a669738693a3cfff0d85d827674
Yuwei Zhou <yz534@drexel.edu>
2018-02-16 10:47:24 -0500

    initialize start window with checkerBoard class

---
+++ Checkers/Checkers/CheckerBoard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    class CheckerBoard
+    {
+        private Array board = new Array[8, 8];
+
+        public CheckerBoard getBoard()
+        {
+            return null;
+        }
+    }
+}
