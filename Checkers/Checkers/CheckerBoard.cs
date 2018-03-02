5875a091fef21e2d5b17da6ec4c2deac2f4b9bc9
Guruansh Singh <singhguruansh@gmail.com>
2018-03-02 13:40:37 -0500

    Added initialization to CheckerBoard

--- Checkers/Checkers/CheckerBoard.cs
+++ Checkers/Checkers/CheckerBoard.cs
@@ -12,23 +12,69 @@ namespace Checkers
 
         public CheckerBoard ()
         {
-        	//board = new int [8][8];
-        	//Populate board
+        	board = new int [8,8];
+
+            for (int i = 0; i<= 2; i++)
+            {
+                for (int j=0; j <= 7; j++)
+                {
+                    if(i%2 == 0 && j % 2 == 1)
+                    {
+                        board[i,j] = 1;
+                    }
+                    else if(i%2 == 1 && j % 2 == 1)
+                    {
+                        board[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 5; i <= 7; i++)
+            {
+                for (int j = 0; j <= 7; j++)
+                {
+                    if (i % 2 == 0 && j % 2 == 1)
+                    {
+                        board[i, j] = 2;
+                    }
+                    else if (i % 2 == 1 && j % 2 == 1)
+                    {
+                        board[i, j] = 2;
+                    }
+                }
+            }
+
         }
 
-        public static CheckerBoard getBoard()
+        public void printBoard()
         {
-            return this.board;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                    Console.Write(board[i, j]);
+                Console.WriteLine();
+            }
+        }
+        public CheckerBoard getBoard()
+        {
+            return this;
         }
 
-        public bool applyMove ()
+        public bool applyMove (String mv)
         {
-        	//Apply Move and return true if move successfully moved
+            String [] pos = mv.Split(',');
+            int[] cleanPos = new int[2];
+            cleanPos[0] = Int32.Parse(pos[0]);
+            cleanPos[1] = Int32.Parse(pos[1]);
+
+
+            //Apply Move and return true if move successfully moved
         }
 
-        private validateMove ()
+        private bool validateMove (int[] move)
         {
-        	//Validate a Given Move
+
+            return false;
         }
 
         private bool getResult(int Player)
