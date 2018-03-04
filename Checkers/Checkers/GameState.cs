cb5c1a068cde3bda82c6d487e98ba110f1e25785
Guruansh Singh <singhguruansh@gmail.com>
2018-03-04 13:14:21 -0500

    Added functionality for Game State

--- Checkers/Checkers/GameState.cs
+++ Checkers/Checkers/GameState.cs
@@ -9,6 +9,12 @@ namespace Checkers
 {
     public class GameState
     {
+        public GameState (string playerName)
+        {
+            this.player1Name = playerName;
+            this._checkerBoard = new CheckerBoard();
+        }
+
         private CheckerBoard _checkerBoard;
         public CheckerBoard cb
         {
@@ -37,9 +43,9 @@ namespace Checkers
 
         private string _player2Name;
 
-        public bool getResult()
+        public int getResult()
         {
-            throw new NotImplementedException();
+            return this._checkerBoard.getResult();
         }
 
         public string player2Name
@@ -53,6 +59,7 @@ namespace Checkers
                 _player2Name = value;
             }
         }
+ 
 
         //Not useful for now
         /*
@@ -71,15 +78,21 @@ namespace Checkers
 
         public int[,] getBoard()
         {
-            int[,] board = new int[8, 8];
-            return board;
+            return this._checkerBoard.getBoard();
         }
 
-        public GameState applyMove(List<int> movePair)
+        public GameState applyMove(List<int> movePair, int playerID)
         {
-            GameState gs = new GameState();
-            return gs;
+            this._checkerBoard.applyMove(movePair, playerID);
+            return this;
         }
 
+        public bool checkAvailableJump(int x, int y, int playerID)
+        {
+            int[] pos = new int[2];
+            pos[0] = x;
+            pos[1] = y;
+            return this._checkerBoard.checkAnyJumpPossiblePiece(pos,playerID);
+        }
     }
 }
